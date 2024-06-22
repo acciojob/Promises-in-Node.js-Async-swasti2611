4b23975191865002eb4b595b9d9c2a00e81419e7
Swati jagtap <swatijagtap5060@gmail.com>
2024-06-22 15:25:13 +0530

    submitted4

--- main.js
+++ main.js
@@ -1,39 +1,42 @@
 const fs = require('fs');
 
 const filePath = process.argv[2];
-if (!filePath || filePath !== 'test.text') {
-  throw new Error('Provided file path does not exist or is incorrect');
-}
-
+if (!filePath || filePath!=='test.txt') {
+  throw new Error(' provided file path does not exist');
+}else{
 function readFileAsync(filePath) {
   return new Promise((resolve, reject) => {
     fs.readFile(filePath, 'utf8', (err, data) => {
       if (err) {
         reject(`Error reading file ${filePath}: ${err}`);
-      } else {
-        resolve(data);
+        return;
       }
+      resolve(data);
     });
   });
 }
-
+}
 function modifyText(text) {
   return new Promise((resolve, reject) => {
-    if (!text || typeof text !== 'string') {
-      reject('Invalid text');
-    } else {
+    // TODO: Convert text to uppercase and reverse it
+    
+    if (!text || typeof text !== "string") {
+      reject("Invalid text");
+      return;
+    }
       let upperCaseText = text.toUpperCase();
       let reversedText = upperCaseText.split('').reverse().join('');
       resolve(reversedText);
-    }
+    
+    
   });
 }
 
 readFileAsync(filePath)
-  .then(data => modifyText(data))
-  .then(modifiedText => {
+  .then((data) => modifyText(data))
+  .then((modifiedText) => {
     console.log(modifiedText);
   })
-  .catch(error => {
+  .catch((error) => {
     console.error(error);
   });
