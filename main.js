be3516653bf77836cd9f708aea398582463d56a1
Swati jagtap <swatijagtap5060@gmail.com>
2024-06-22 15:52:14 +0530

    submitted7

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 const fs = require('fs');
 const path=require('path')
 const filePath = process.argv[2];
- if(!filePath){
-  throw new Error('Provided file path does not exist or is incorrect');
-  return ;
- }
+ 
 function readFileAsync(filePath) {
+
+  if (!filePath) {
+    console.error('Error: Please provide the file path as an argument');
+    return;
+}
   const absolutePath = path.resolve(filePath);
   return new Promise((resolve, reject) => {
     fs.readFile(filePath, 'utf8', (err, data) => {
